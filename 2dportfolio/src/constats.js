52a1946dda50c7dfa7f3e30073c5c0982359e0ce
Aditya Kamble <122638989+27-aditya@users.noreply.github.com>
2024-04-23 18:58:55 +0530

    Added code files

---
+++ 2dportfolio/src/constats.js
@@ -0,0 +1,17 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work in C/C++, Python, HTML, CSS, JavaScript. I am also learning Rust. Anyway regardless of the lanugage, I like to tinker around with existing code and also build something new on my own.
+    Here is my <a href="https://github.com/27-aditya" target="_blank">Github</a>!`,
+  "cs-degree": `This is where I will hang my CS degree. It is feels like a balanced degree to me. I've learned a lot of things in college but I've learned more from the internet.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+  I like to watch Fireship and GothamChess videos on youtube. Also I like to watch anime.`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@InternetOfBugs" target="_blank">Internet Of Bugs</a>, <a href="https://www.youtube.com/@Telusko" target="_blank">Telusko</a>,
+  <a href="https://www.youtube.com/@harkirat1" target="_blank">Harkirat Singh</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  bed: `This where I sleep. As a wise man once said, "When things get hard take a break, take a nap". It helps me solve my problems by refreshing my brain.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/27-aditya" target="_blank">Check it out?</a>
+  Contact me at kambleaditya946.com if you have any interesting job opportunities!`,
+  library: `There are a lot of books on my shelves not just the educational ones but a wide variety of books including fictional, non-fictional, self-help, I even have some mangas in there. I like to read books that pique my interest within a few pages of the beginning.`,
+  exit: `If you want to exit Aditya's portfolio, just close the tab.`,
+};
